1c006ce316fe3992f3bbcd6fee6ea8148cf06f14
Steven Waterman <swaterman@scottlogic.com>
2019-08-27 16:24:03 +0100

    Tidy up StatefulActionCreator

--- client/src/util/StatefulActionCreator.ts
+++ client/src/util/StatefulActionCreator.ts
@@ -2,32 +2,25 @@ import {Action} from "redux";
 import {ThunkAction, ThunkDispatch} from "redux-thunk"
 import {State} from "../state/store/RootStore";
 
-export function fire<P, A extends Action<any>>(dispatch: ThunkDispatch<State, void, A>, create: (state: State, param: P) => A | null): (params: P) => void {
+type Dispatch<A extends Action<any>> = ThunkDispatch<State, void, A>
+type Creator<A extends Action<any>, P> = (state: State, param: P, dispatch: Dispatch<A>) => A | null;
+type ThunkAct<A extends Action<any>> = ThunkAction<void, State, void, A>
+
+export function fire<P, A extends Action<any>>(dispatch: Dispatch<A>, creator: Creator<A, P>): (params: P) => void {
     return (params: P) => {
-        const thunkAction = actionCreator(create)(params);
+        const thunkAction = actionCreator(creator)(params);
         dispatch(thunkAction);
     }
 }
 
-function actionCreator<P, A extends Action<any>>(creatorFunction: (state: State, params: P) => A | null): (params: P) => ThunkAction<void, State, void, A> {
-    return (params: P) => (dispatch: ThunkDispatch<State, void, A>, getState: () => State) => {
-        const action: A | null = creatorFunction(getState(), params);
-        if (action != null) {
-            dispatch(action);
-        }
-    }
+export function fireNP<A extends Action<any>>(dispatch: Dispatch<A>, creator: Creator<A, null>): () => void {
+    const func = fire<null, A>(dispatch, creator);
+    return () => func(null);
 }
 
-export function fireNP<A extends Action<any>>(dispatch: ThunkDispatch<State, void, A>, create: (state: State) => A | null): () => void {
-    return () => {
-        const thunkAction = actionCreatorNP(create)();
-        dispatch(thunkAction);
-    }
-}
-
-function actionCreatorNP<A extends Action<any>>(creatorFunction: (state: State) => A | null): () => ThunkAction<void, State, void, A> {
-    return () => (dispatch: ThunkDispatch<State, void, A>, getState: () => State) => {
-        const action: A | null = creatorFunction(getState());
+function actionCreator<P, A extends Action<any>>(creator: Creator<A, P>): (params: P) => ThunkAct<A> {
+    return (params: P) => (dispatch, getState) => {
+        const action = creator(getState(), params, dispatch);
         if (action != null) {
             dispatch(action);
         }
