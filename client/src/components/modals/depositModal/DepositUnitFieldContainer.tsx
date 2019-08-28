98d0d8dad8a32dedbf7f22a28cde7827d608015e
Steven Waterman <swaterman@scottlogic.com>
2019-08-28 11:00:20 +0100

    now it compiles

--- client/src/components/modals/depositModal/DepositUnitFieldContainer.tsx
+++ client/src/components/modals/depositModal/DepositUnitFieldContainer.tsx
@@ -2,10 +2,10 @@ import {connect} from "react-redux";
 import {State} from "../../../modules/RootStore";
 import {ThunkDispatch} from "redux-thunk"
 import NumberField from "../../NumberField";
+import {fire, fireNP, ThunkDsp} from "../../../util/Thunker";
 import DepositModalSetUnitsAction, {createDepositModalSetUnitsAction} from "../../../modules/modals/deposit/input/DepositModalSetUnitsAction";
 import DepositModalResetUnitsTextAction, {createDepositModalResetUnitsTextAction} from "../../../modules/modals/deposit/input/DepositModalResetUnitsTextAction";
 import DepositModalSetUnitsTextAction, {createDepositModalSetUnitsTextAction} from "../../../modules/modals/deposit/input/DepositModalSetUnitsTextAction";
-import {fire, fireNP, ThunkDsp} from "../../../util/Thunker";
 
 type Actions = DepositModalSetUnitsAction | DepositModalSetUnitsTextAction | DepositModalResetUnitsTextAction
 
